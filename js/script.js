1a5ae0d3da376bc16724b4b279182bd98fd116c8
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2023-11-28 12:29:57 +0200

    callback

--- js/script.js
+++ js/script.js
@@ -1,69 +1,22 @@
 "use strict";
 
-// function findLargestNumber() {
-//   let max;
-//   for (const num of arguments) {
-//     if (typeof num === "number") {
-//       max = num;
-//       break;
-//     }
-//   }
-//   for (const num of arguments) {
-//     if (typeof num === "number") {
-//       if (num > max) {
-//         max = num;
-//       }
-//     } else {
-//       max = findLargestNumber(...num);
-//     }
-//   }
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-//   return max;
-// }
-
-// console.log(findLargestNumber([[1, 143], 5, 7], 6, 7, 8, 88, 123));
-
-// const product = {
-//     id, name, price, quantity,
-// }
-
-const product1 = { id: 1, name: "Apple", price: 0.5, quantity: 30 };
-const product2 = { id: 2, name: "Banana", price: 0.3, quantity: 50 };
-
-let inventory = [];
-
-function addProduct(product) {
-  return inventory.push(product);
-}
-
-function findProductById(id) {
-  for (const product of inventory) {
-    if (product.id === id) {
-      return product;
-    }
+function each(arr, callback) {
+  let total = arr[0];
+  for (let i = 1; i < arr.length; i += 1) {
+    total = callback(total, arr[i]);
   }
-  return null;
+  return total;
 }
 
-function updateProductQuantity(id, newQuantity) {
-  for (const product of inventory) {
-    if (product.id === id) {
-      product.quantity = newQuantity;
-    }
-  }
+function add(a, b) {
+  return a + b;
 }
 
-function listAllProducts() {
-  return console.log(inventory);
+function mult(a, b) {
+  return a * b;
 }
 
-addProduct({ id: 1, name: "Apple", price: 0.5, quantity: 30 });
-addProduct({ id: 2, name: "Banana", price: 0.3, quantity: 50 });
-console.log(inventory);
-
-console.log(findProductById(2));
-
-updateProductQuantity(2, 45);
-console.log(inventory);
-
-listAllProducts();
+console.log(each(arr, add));
+console.log(each(arr, mult));
