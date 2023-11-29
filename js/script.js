7ca7f4af15afb74049181640ef0edf95b8a28d51
DevMasterX <sokol080288@gmail.com>
2023-11-29 07:01:12 +0200

    1

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 "use strict";
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function each(arr, callback) {
-  let total = arr[0];
-  for (let i = 1; i < arr.length; i += 1) {
-    total = callback(total, arr[i]);
-  }
-  return total;
+// function each(arr, callback) {
+//   let total = arr[0];
+//   for (let i = 1; i < arr.length; i += 1) {
+//     total = callback(total, arr[i]);
+//   }
+//   return total;
+// }
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function mult(a, b) {
+//   return a * b;
+// }
+
+// console.log(each(arr, add));
+// console.log(each(arr, mult));
+
+function handleSuccess(message) {
+  console.log(`Success! ${message}`);
 }
-
-function add(a, b) {
-  return a + b;
-}
-
-function mult(a, b) {
-  return a * b;
-}
-
-console.log(each(arr, add));
-console.log(each(arr, mult));
