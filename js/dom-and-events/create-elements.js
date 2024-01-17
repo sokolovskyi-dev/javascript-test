5efc42ea2a1be0219ff9356ba2f71e17926f180c
DevMasterX <sokol080288@gmail.com>
2024-01-18 00:16:38 +0200

    dom2

--- js/dom-and-events/create-elements.js
+++ js/dom-and-events/create-elements.js
@@ -1,4 +1,31 @@
 const titleEl = document.createElement("h1");
 titleEl.classList.add("page-title");
 titleEl.textContent = "Это заголовок страницы";
-console.log("🚀  titleEl:", titleEl);
+
+document.body.appendChild(titleEl);
+
+const imageEl = document.createElement("img");
+imageEl.src = "https://tagil.masmotors.ru/colors/peugeot-5008-old/2.png";
+imageEl.alt = "peugeot-5008";
+imageEl.width = 440;
+
+document.body.appendChild(imageEl);
+
+const navItemEl = document.createElement("li");
+navItemEl.classList.add("site-nav-iitem");
+
+const navLinkEl = document.createElement("a");
+navLinkEl.classList.add("site-navlink");
+navLinkEl.textContent = "Личный кабинет";
+navLinkEl.href = "/profile";
+
+navItemEl.appendChild(navLinkEl);
+
+const navEl = document.querySelector(".site-nav");
+
+navEl.insertBefore(navItemEl, navEl.firstElementChild);
+
+const heroEl = document.querySelector(".hero");
+// heroEl.appendChild(titleEl);
+// heroEl.appendChild(imageEl);
+heroEl.append(titleEl, imageEl);
