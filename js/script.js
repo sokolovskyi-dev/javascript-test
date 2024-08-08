430d1525529022f38da2b5d026611c09f0014a1a
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2024-08-08 12:27:54 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1042,15 +1042,3 @@
 //   totalSalary += salary;
 // }
 // console.log(totalSalary);
-
-const min = 0;
-const max = 5;
-let total = 0;
-
-for (let i = min; i <= max; i += 1) {
-  if (i % 2 === 0) {
-    total += i;
-  }
-}
-
-console.log("total:", total);
