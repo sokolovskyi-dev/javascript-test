85b0a009d752a9b144859e41750f102887fd0ea8
DevMasterX <sokol080288@gmail.com>
2024-10-18 00:03:54 +0300

    879

--- js/script.js
+++ js/script.js
@@ -2049,45 +2049,49 @@
 // mango.attack();
 // mango.gainXP(1000);
 
-class Component {
-  constructor(selector) {
-    this.$el = document.querySelector(selector);
-  }
+// class Component {
+//   constructor(selector) {
+//     this.$el = document.querySelector(selector);
+//   }
 
-  hide() {
-    this.$el.style.display = "none";
-  }
+//   hide() {
+//     this.$el.style.display = "none";
+//   }
 
-  show() {
-    this.$el.style.display = "block";
-  }
-}
+//   show() {
+//     this.$el.style.display = "block";
+//   }
+// }
 
-class Box extends Component {
-  constructor(options) {
-    super(options.selector);
-    this.$el.style.width = this.$el.style.height = options.size + "px";
-    this.$el.style.background = options.color;
-  }
-}
+// class Box extends Component {
+//   constructor(options) {
+//     super(options.selector);
+//     this.$el.style.width = this.$el.style.height = options.size + "px";
+//     this.$el.style.background = options.color;
+//   }
+// }
 
-const box1 = new Box({
-  selector: "#box1",
-  size: 100,
-  color: "red",
-});
+// const box1 = new Box({
+//   selector: "#box1",
+//   size: 100,
+//   color: "red",
+// });
 
-box1.hide();
-box1.show();
+// box1.hide();
+// box1.show();
 
-const box2 = new Box({
-  selector: "#box2",
-  size: 120,
-  color: "blue",
-});
+// const box2 = new Box({
+//   selector: "#box2",
+//   size: 120,
+//   color: "blue",
+// });
 
-class circle extends Box {
-  constructor(options) {
-    super(options);
-  }
+// class circle extends Box {
+//   constructor(options) {
+//     super(options);
+//   }
+// }
+
+class Animal {
+  constructor(options) {}
 }
