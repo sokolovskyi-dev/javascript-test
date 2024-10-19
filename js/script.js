d9df092a3a96164cc5fcb4c4571b7a6fae05746c
DevMasterX <sokol080288@gmail.com>
2024-10-20 01:54:35 +0300

    545949

--- js/script.js
+++ js/script.js
@@ -2194,3 +2194,37 @@ const linkEl = document.querySelector(
 );
 
 linkEl.classList.add("site-nav-link-current");
+
+const colorPickerOptions = [
+  { label: "red", color: "#F44336" },
+  { label: "green", color: "#4CAF50" },
+  { label: "blue", color: "#2196F3" },
+  { label: "grey", color: "#60708B" },
+  { label: "pink", color: "#E91E63" },
+  { label: "indigo", color: "#3F51B5" },
+];
+
+const colorPickerContainerEl = document.querySelector(".js-color-picker");
+
+// const elements = colorPickerOptions.map((option) => {
+//   const buttonEl = document.createElement("button");
+//   buttonEl.classList.add("color-picker-option");
+//   buttonEl.textContent = option.label;
+//   buttonEl.style.backgroundColor = option.color;
+
+//   return buttonEl;
+// });
+
+const makeColorPickerOptions = (options) => {
+  return options.map((option) => {
+    const buttonEl = document.createElement("button");
+    buttonEl.classList.add("color-picker-option");
+    buttonEl.textContent = option.label;
+    buttonEl.style.backgroundColor = option.color;
+
+    return buttonEl;
+  });
+};
+
+const elements = makeColorPickerOptions(colorPickerOptions);
+colorPickerContainerEl.append(...elements);
