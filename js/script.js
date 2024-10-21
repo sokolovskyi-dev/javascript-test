92a023dbf6bfa90ec50545708bd6235ef8070790
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2024-10-21 09:59:31 +0300

    2435

--- js/script.js
+++ js/script.js
@@ -2283,3 +2283,21 @@
 // titleEl.insertAdjacentHTML("beforeend", "<a href=''>Это ссылка)</a>");
 
 // console.log(titleEl);
+
+const targetBtn = document.querySelector(".js-target-btn");
+const addListenerBtn = document.querySelector(".js-add-listener");
+const removeListenerBtn = document.querySelector(".js-remove-listener");
+
+addListenerBtn.addEventListener("click", () => {
+  console.log("Добавляю слушателя событий");
+  targetBtn.addEventListener("click", onTargetBtnClick);
+});
+
+removeListenerBtn.addEventListener("click", () => {
+  console.log("Снимаю слушателя");
+  targetBtn.removeEventListener("click", onTargetBtnClick);
+});
+
+function onTargetBtnClick() {
+  console.log("Клик по целевой кнопке");
+}
