85f0af174105d1dda0055b9974f2d56410f0cc06
DevMasterX <sokol080288@gmail.com>
2024-10-25 00:00:39 +0300

    54654

--- js/script.js
+++ js/script.js
@@ -2403,87 +2403,87 @@
 // function onSubmit(event) {
 //   event.preventDefault();
 // }
-const cars = [
-  {
-    id: 1,
-    car: "Honda",
-    type: "Civic",
-    price: 12000,
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTCOHzdE-dK6WK7ax8NzQolTcCWA_jhJD-CRGWfqKJIJuGs8ML_-OyiDwzsdC8jOi_K10&usqp=CAU",
-  },
-  {
-    id: 2,
-    car: "Audi",
-    type: "Q7",
-    price: 40000,
-    img: "https://upload.wikimedia.org/wikipedia/commons/8/8b/2017_Audi_Q7_S_Line_Quattro_3.0_Front.jpg",
-  },
-  {
-    id: 33,
-    car: "BMW",
-    type: "5 siries",
-    price: 9000,
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUH96e58ynLO8SXMsFTNYkJci79eAZ8CyqcZsZ8snvzz2sfLl3Ojd1BQoaWBcrMKWvSYc&usqp=CAU",
-  },
-  {
-    id: 3,
-    car: "Honda",
-    type: "Accord",
-    price: 20000,
-    number: "+380000000000",
-    img: "https://upload.wikimedia.org/wikipedia/commons/7/76/2021_Honda_Accord_Sport_%28facelift%29%2C_front_11.30.21.jpg",
-  },
-  {
-    id: 4,
-    car: "Volvo",
-    type: "XC60",
-    price: 7000,
-    img: "https://www.volvocars.com/media/shared-assets/master/images/pages/my19/xc60-my19/accessories/xc60my19_accessories_exteriorfeature2_1.jpg?w=320",
-  },
-];
+// const cars = [
+//   {
+//     id: 1,
+//     car: "Honda",
+//     type: "Civic",
+//     price: 12000,
+//     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTCOHzdE-dK6WK7ax8NzQolTcCWA_jhJD-CRGWfqKJIJuGs8ML_-OyiDwzsdC8jOi_K10&usqp=CAU",
+//   },
+//   {
+//     id: 2,
+//     car: "Audi",
+//     type: "Q7",
+//     price: 40000,
+//     img: "https://upload.wikimedia.org/wikipedia/commons/8/8b/2017_Audi_Q7_S_Line_Quattro_3.0_Front.jpg",
+//   },
+//   {
+//     id: 33,
+//     car: "BMW",
+//     type: "5 siries",
+//     price: 9000,
+//     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUH96e58ynLO8SXMsFTNYkJci79eAZ8CyqcZsZ8snvzz2sfLl3Ojd1BQoaWBcrMKWvSYc&usqp=CAU",
+//   },
+//   {
+//     id: 3,
+//     car: "Honda",
+//     type: "Accord",
+//     price: 20000,
+//     number: "+380000000000",
+//     img: "https://upload.wikimedia.org/wikipedia/commons/7/76/2021_Honda_Accord_Sport_%28facelift%29%2C_front_11.30.21.jpg",
+//   },
+//   {
+//     id: 4,
+//     car: "Volvo",
+//     type: "XC60",
+//     price: 7000,
+//     img: "https://www.volvocars.com/media/shared-assets/master/images/pages/my19/xc60-my19/accessories/xc60my19_accessories_exteriorfeature2_1.jpg?w=320",
+//   },
+// ];
 
-const listEl = document.querySelector(".js-list");
-const formEl = document.querySelector(".js-search-form");
-const resetBtnEl = document.querySelector(".js-reset");
+// const listEl = document.querySelector(".js-list");
+// const formEl = document.querySelector(".js-search-form");
+// const resetBtnEl = document.querySelector(".js-reset");
 
-resetBtnEl.addEventListener("click", onReset);
-formEl.addEventListener("submit", onSearch);
+// resetBtnEl.addEventListener("click", onReset);
+// formEl.addEventListener("submit", onSearch);
 
-function onReset() {
-  listEl.insertAdjacentHTML("beforeend", createMarkup(cars));
-  formEl.elements.query.value = "";
-}
+// function onReset() {
+//   listEl.insertAdjacentHTML("beforeend", createMarkup(cars));
+//   formEl.elements.query.value = "";
+// }
 
-function createMarkup(arr) {
-  return arr
-    .map(
-      ({ id, car, type, price, img }) => `
-<li data-id="${id}">
-        <img src="${img}" alt="${car}" width = '300' >
-        <h2>${car}</h2>
-        <h3>${type}</h3>
-        <p>${price}</p>
-      </li>
-`
-    )
-    .join("");
-}
+// function createMarkup(arr) {
+//   return arr
+//     .map(
+//       ({ id, car, type, price, img }) => `
+// <li data-id="${id}">
+//         <img src="${img}" alt="${car}" width = '300' >
+//         <h2>${car}</h2>
+//         <h3>${type}</h3>
+//         <p>${price}</p>
+//       </li>
+// `
+//     )
+//     .join("");
+// }
 
-listEl.insertAdjacentHTML("beforeend", createMarkup(cars));
+// listEl.insertAdjacentHTML("beforeend", createMarkup(cars));
 
-function onSearch(event) {
-  event.preventDefault();
-  const form = event.currentTarget;
-  // console.dir(form.elements);
+// function onSearch(event) {
+//   event.preventDefault();
+//   const form = event.currentTarget;
+//   // console.dir(form.elements);
 
-  const { query, select } = form.elements;
-  console.dir(query.value);
-  console.dir(select.value);
-  const searchCars = cars.filter(
-    (item) =>
-      item[select.value].toLowerCase() === query.value.trim().toLowerCase()
-  );
-  // console.log("🚀  searchCars:", searchCars);
+//   const { query, select } = form.elements;
+//   console.dir(query.value);
+//   console.dir(select.value);
+//   const searchCars = cars.filter(
+//     (item) =>
+//       item[select.value].toLowerCase() === query.value.trim().toLowerCase()
+//   );
+//   // console.log("🚀  searchCars:", searchCars);
 
-  listEl.innerHTML = createMarkup(searchCars);
-}
+//   listEl.innerHTML = createMarkup(searchCars);
+// }
