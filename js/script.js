d062f38f5e4be7adff43ffb0ad328b6f89d4419b
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2024-10-29 12:40:34 +0200

    5665

--- js/script.js
+++ js/script.js
@@ -2534,5 +2534,32 @@
 //   input.blur();
 //   input.value = "";
 // }
-const input = document.querySelector("input");
-console.log("🚀  input:", input);
+
+// const colorPickerOptions = [
+//   { label: "red", color: "#F44336" },
+//   { label: "green", color: "#4CAF50" },
+//   { label: "blue", color: "#2196F3" },
+//   { label: "grey", color: "#607D8B" },
+//   { label: "pink", color: "#E91E63" },
+//   { label: "indigo", color: "#3F51B5" },
+// ];
+
+// const colorPickerContainerEl = document.querySelector(".js-color-picker");
+
+// /*
+//  * Пишем функцию для создания разметки колорпикера
+//  */
+// const makeColorPickerOptions = (options) => {
+//   return options.map((option) => {
+//     const buttonEl = document.createElement("button");
+//     buttonEl.type = "button";
+//     buttonEl.classList.add("color-picker__option");
+//     buttonEl.textContent = option.label;
+//     buttonEl.style.backgroundColor = option.color;
+
+//     return buttonEl;
+//   });
+// };
+
+// const elements = makeColorPickerOptions(colorPickerOptions);
+// colorPickerContainerEl.append(...elements);
