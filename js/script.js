c03f8ff38d4c5d40d72e49eda0694ad829726bd4
DevMasterX <sokol080288@gmail.com>
2024-11-01 23:27:52 +0200

    46546

--- js/script.js
+++ js/script.js
@@ -2673,48 +2673,48 @@
 //   player = player === "X" ? "0" : "X";
 // }
 
-function isWinner() {}
-const cars = [
-  {
-    // id: 1,
-    car: "Honda",
-    type: "Civic",
-    price: 12000,
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTCOHzdE-dK6WK7ax8NzQolTcCWA_jhJD-CRGWfqKJIJuGs8ML_-OyiDwzsdC8jOi_K10&usqp=CAU",
-  },
-  {
-    id: 2,
-    car: "Audi",
-    type: "Q7",
-    price: 40000,
-    img: "https://upload.wikimedia.org/wikipedia/commons/8/8b/2017_Audi_Q7_S_Line_Quattro_3.0_Front.jpg",
-  },
-  {
-    id: 3,
-    car: "BMW",
-    type: "5 siries",
-    price: 9000,
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUH96e58ynLO8SXMsFTNYkJci79eAZ8CyqcZsZ8snvzz2sfLl3Ojd1BQoaWBcrMKWvSYc&usqp=CAU",
-  },
-  {
-    id: 4,
-    car: "Honda",
-    type: "Accord",
-    price: 20000,
-    number: "+380000000000",
-    img: "https://upload.wikimedia.org/wikipedia/commons/7/76/2021_Honda_Accord_Sport_%28facelift%29%2C_front_11.30.21.jpg",
-  },
-  {
-    id: 5,
-    car: "Volvo",
-    type: "XC60",
-    price: 7000,
-    img: "https://www.volvocars.com/media/shared-assets/master/images/pages/my19/xc60-my19/accessories/xc60my19_accessories_exteriorfeature2_1.jpg?w=320",
-  },
-];
+// function isWinner() {}
+// const cars = [
+//   {
+//     // id: 1,
+//     car: "Honda",
+//     type: "Civic",
+//     price: 12000,
+//     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTCOHzdE-dK6WK7ax8NzQolTcCWA_jhJD-CRGWfqKJIJuGs8ML_-OyiDwzsdC8jOi_K10&usqp=CAU",
+//   },
+//   {
+//     id: 2,
+//     car: "Audi",
+//     type: "Q7",
+//     price: 40000,
+//     img: "https://upload.wikimedia.org/wikipedia/commons/8/8b/2017_Audi_Q7_S_Line_Quattro_3.0_Front.jpg",
+//   },
+//   {
+//     id: 3,
+//     car: "BMW",
+//     type: "5 siries",
+//     price: 9000,
+//     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUH96e58ynLO8SXMsFTNYkJci79eAZ8CyqcZsZ8snvzz2sfLl3Ojd1BQoaWBcrMKWvSYc&usqp=CAU",
+//   },
+//   {
+//     id: 4,
+//     car: "Honda",
+//     type: "Accord",
+//     price: 20000,
+//     number: "+380000000000",
+//     img: "https://upload.wikimedia.org/wikipedia/commons/7/76/2021_Honda_Accord_Sport_%28facelift%29%2C_front_11.30.21.jpg",
+//   },
+//   {
+//     id: 5,
+//     car: "Volvo",
+//     type: "XC60",
+//     price: 7000,
+//     img: "https://www.volvocars.com/media/shared-assets/master/images/pages/my19/xc60-my19/accessories/xc60my19_accessories_exteriorfeature2_1.jpg?w=320",
+//   },
+// ];
 
-const container = document.querySelector(".js-container");
-// {
+// const container = document.querySelector(".js-container");
+// // {
 //     id: 1,
 //     car: "Honda",
 //     type: "Civic",
@@ -2725,42 +2725,59 @@ const container = document.querySelector(".js-container");
   /* <li><img src="" alt=""><h2></h2></li> */
 }
 
-const markup = cars.map(
-  ({ img, car, id }) =>
-    `<li data-car-id="${id}"><img class='js-target' src="${img}" alt="${car}" width='200'><h2 class='js-target'>${car}</h2 </li>`
-);
+// const markup = cars.map(
+//   ({ img, car, id }) =>
+//     `<li data-car-id="${id}"><img class='js-target' src="${img}" alt="${car}" width='200'><h2 class='js-target'>${car}</h2 </li>`
+// );
 
-container.insertAdjacentHTML("beforeend", markup.join(""));
+// container.insertAdjacentHTML("beforeend", markup.join(""));
 
-container.addEventListener("click", onClick);
+// container.addEventListener("click", onClick);
 
-function onClick(e) {
-  if (!e.target.classList.contains("js-target")) {
-    return;
-  }
+// function onClick(e) {
+//   if (!e.target.classList.contains("js-target")) {
+//     return;
+//   }
 
-  const carId = e.target.closest("li").dataset.carId;
+//   const carId = e.target.closest("li").dataset.carId;
 
-  const currentItem = cars.find(({ id }) => id === Number(carId));
+//   const currentItem = cars.find(({ id }) => id === Number(carId));
 
-  if (!currentItem) {
-    const instance = basicLightbox.create(`
-    <div>
-        <img src="https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Glossary.svg" width='300' alt="placeholder" />
-        
-      </div>
-`);
-    instance.show();
-    return;
-  }
+//   if (!currentItem) {
+//     const instance = basicLightbox.create(`
+//     <div>
+//         <img src="https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Glossary.svg" width='300' alt="placeholder" />
 
-  const instance = basicLightbox.create(`
-    <div>
-        <img src="${currentItem.img}" width='300' alt="${currentItem.car}" />
-        <h2>${currentItem.car}</h2>
-        <h3>${currentItem.type}</h3>
-        <p>${currentItem.price}</p>
-      </div>
-`);
-  instance.show();
+//       </div>
+// `);
+//     instance.show();
+//     return;
+//   }
+
+//   const instance = basicLightbox.create(`
+//     <div>
+//         <img src="${currentItem.img}" width='300' alt="${currentItem.car}" />
+//         <h2>${currentItem.car}</h2>
+//         <h3>${currentItem.type}</h3>
+//         <p>${currentItem.price}</p>
+//       </div>
+// `);
+//   instance.show();
+// }
+
+// let str1 = "abc";
+// let str2 = "foo";
+
+// let uniqueLetters1 = new Set(str1);
+// console.log(uniqueLetters1.size);
+// let uniqueLetters2 = new Set(str2);
+
+// console.log(uniqueLetters2.size);
+
+// // from GPT
+
+const arr = ["abc", "acb", "bac", "foo", "bca", "cab", "cba"];
+
+for (const element of arr) {
+  console.log("🚀  element:", element);
 }
